7b2fc9c8c7fbd20d25923e4085a479bf0e5b12b9
crontreras1 <crontreras1@gmail.com>
2024-01-22 10:16:33 -0500

    class 2. class

---
+++ src/01Class.ts
@@ -0,0 +1,19 @@
+const date: Date = new Date()
+
+const newDate = date.getMinutes()
+console.log(newDate)
+
+class MyDate {
+    year: number
+    month: number
+    day: number
+
+    constructor (year: number, month: number, day: number) {
+        this.year = year
+        this.month = month
+        this.day = day
+    }
+}
+
+const myDate = new MyDate(1992, 6, 11)
+console.log(myDate);
